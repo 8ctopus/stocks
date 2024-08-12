1604d36d0aca8d2588989107ca48fafdc96aed22
8ctopus <hello@octopuslabs.io>
2024-08-12 10:46:49 +0400

    Add portfolio class Add portfolio load and save to file

---
+++ src/Portfolio.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oct8pus\Stocks;
+
+use ArrayIterator;
+use Exception;
+use IteratorAggregate;
+use Traversable;
+
+class Portfolio implements IteratorAggregate
+{
+    private static string $file = __DIR__ . '/../portfolio.dat';
+    private array $positions;
+
+    public function __construct()
+    {
+        $this->positions = [];
+    }
+
+    public function add(Position $position) : self
+    {
+        $this->positions[] = $position;
+        return $this;
+    }
+    public function currentValue() : float
+    {
+        throw new Exception('not implemented');
+    }
+
+    public function report(string $type) : string
+    {
+        switch ($type) {
+            default:
+                throw new Exception('not implemented');
+        }
+    }
+
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->positions);
+    }
+
+    public static function load() : self
+    {
+        return unserialize(file_get_contents(self::$file));
+    }
+
+    public function save() : void
+    {
+        file_put_contents(self::$file, serialize($this));
+    }
+}
