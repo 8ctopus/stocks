be2f3b2da22cd56d52756aa9e10f0d4fa3a6b66f
8ctopus <hello@octopuslabs.io>
2024-08-19 19:04:49 +0400

    Fix position cleanup

---
+++ src/Cli/PositionCleanup.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Oct8pus\Stocks\Cli;
+
+use Swew\Cli\Command;
+
+class PositionCleanup extends Command
+{
+    const NAME = 'cleanup';
+    const DESCRIPTION = 'Cleanup position';
+
+    public function __invoke() : int
+    {
+        $commander = $this->getCommander();
+        $portfolio = $commander->portfolio();
+
+        foreach ($portfolio as $position) {
+            $position->cleanup();
+        }
+
+
+        $portfolio->save();
+
+        return self::SUCCESS;
+    }
+}
