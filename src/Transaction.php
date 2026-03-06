80dd3a654e507a4873dffdc693357ddb93d0fc35
8ctopus <hello@octopuslabs.io>
2026-03-06 09:54:35 +0400

    Add transaction explicit serialization and de-serialization Add transaction cost

--- src/Transaction.php
+++ src/Transaction.php
@@ -11,12 +11,14 @@ class Transaction
     private readonly DateTime $date;
     private readonly int $shares;
     private readonly float $price;
+    private readonly float $cost;
 
-    public function __construct(DateTime $date, int $shares, float $price)
+    public function __construct(DateTime $date, int $shares, float $price, float $cost = 0)
     {
         $this->date = $date;
         $this->shares = $shares;
         $this->price = $price;
+        $this->cost = $cost;
     }
 
     public function __toString() : string
@@ -24,7 +26,7 @@ class Transaction
         $date = $this->date->format('d.m.Y');
         $shares = Helper::sprintf('%5d', $this->shares);
         $price = Helper::sprintf('%6.2f', $this->price);
-        $total = str_pad(number_format($this->value(), 0, '.', '\''), 8, ' ', STR_PAD_LEFT);
+        $total = str_pad(number_format($this->value(false), 0, '.', '\''), 8, ' ', STR_PAD_LEFT);
 
         return "{$date} {$shares} * {$price} = {$total}\n";
     }
@@ -44,9 +46,20 @@ class Transaction
         return $this->price;
     }
 
-    public function value() : float
+    public function cost() : float
     {
-        return $this->price * $this->shares;
+        return $this->cost;
+    }
+
+    public function value(bool $cost) : float
+    {
+        $value = $this->price * $this->shares;
+
+        if (!$cost) {
+            return $value;
+        }
+
+        return $value > 0 ? $value + $this->cost : $value - $this->cost;
     }
 
     public function data() : array
@@ -57,7 +70,25 @@ class Transaction
             '*',
             $this->price,
             '=',
-            (int) round($this->value(), 0, PHP_ROUND_HALF_UP),
+            (int) round($this->value(false), 0, PHP_ROUND_HALF_UP),
         ];
+    }
+
+    public function __serialize() : array
+    {
+        return [
+            'date' => $this->date,
+            'shares' => $this->shares,
+            'price' => $this->price,
+            'cost' => $this->cost,
+        ];
+    }
+
+    public function __unserialize(array $data) : void
+    {
+        $this->date = $data['date'];
+        $this->shares = $data['shares'];
+        $this->price = $data['price'];
+        $this->cost = $data['cost'];
     }
 }
